943cd98008254480a1dbb66eca258eb5127225b8
Courtney Newcomer <courtney.j.newcomer@gmail.com>
2023-07-30 16:17:31 -0500

    exercises

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+starting_height = 100
+bounce_count = 1
+# bounce_height will need to be reassigned after each bounce
+# to reflect height change
+
+while bounce_count < 11:
+    bounce_height = (3/5) * starting_height
+    print(bounce_count, round(bounce_height, 3))
+    starting_height = bounce_height
+    bounce_count += 1
